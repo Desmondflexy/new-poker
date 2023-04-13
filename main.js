cb8597d3071ed65aa8720c7cdc06a49ce6289a2f
Desmondflexy <ikilodesmond@gmail.com>
2023-04-13 09:32:19 +0100

    update

--- main.js
+++ main.js
@@ -20,8 +20,6 @@ class Player {
     }
 }
 
-startGame();
-
 function startGame() {
     const deck_of_cards = full_deck();
 
@@ -29,7 +27,42 @@ function startGame() {
     const p2 = new Player(deck_of_cards);
     const market = createCards(4, document.querySelectorAll('.cards')[2]);
     market.forEach(button => {
-        button.addEventListener('click', pick_card);
+        button.addEventListener('click', function () {
+            const timeout = 300;  // delay in seconds after button click
+            if (turn === 'Player 1') {
+                p1.choose_card(this.dataset.cardvalue);
+                const p_html = createCards(1, document.querySelectorAll('.cards')[0]);
+                cardHtml(this.dataset.cardvalue, p_html[0]);
+                turn = 'Player 2';
+                setTimeout(() => {
+                    document.getElementById('p1').innerHTML = '';
+                    document.getElementById('p2').innerHTML = '(active)';
+                }, timeout);
+            } else {
+                p2.choose_card(this.dataset.cardvalue);
+                const p_html = createCards(1, document.querySelectorAll('.cards')[1]);
+                cardHtml(this.dataset.cardvalue, p_html[0]);
+                turn = 'Player 1';
+                setTimeout(() => {
+                    document.getElementById('p1').innerHTML = '(active)';
+                    document.getElementById('p2').innerHTML = '';
+                }, timeout);
+            }
+            document.querySelectorAll('.pick > button').forEach(button => button.disabled = true);
+
+            setTimeout(() => {
+                document.querySelectorAll('.pick > button').forEach(button => button.disabled = false);
+                generate_random_cards();
+                if (p2.hand.length === 5) {
+                    document.querySelectorAll('.pick > button').forEach(button => button.remove());
+                    pokerHands(p1.hand, p2.hand);
+                }
+            }, timeout);
+
+            setTimeout(() => market.forEach(button => {
+                if (button !== this) flipCard.call(button);
+            }), timeout / 2)
+        });
         button.addEventListener('click', flipCard);
     })
 
@@ -39,43 +72,6 @@ function startGame() {
 
     document.querySelector('.button > button').addEventListener('click', restartGame);
 
-    function pick_card() {
-        const timeout = 1000;
-        if (turn === 'Player 1') {
-            p1.choose_card(this.dataset.cardvalue);
-            const p_html = createCards(1, document.querySelectorAll('.cards')[0]);
-            cardHtml(this.dataset.cardvalue, p_html[0]);
-            turn = 'Player 2';
-            setTimeout(() => {
-                document.getElementById('p1').innerHTML = '';
-                document.getElementById('p2').innerHTML = '(active)';
-            }, timeout);
-        } else {
-            p2.choose_card(this.dataset.cardvalue);
-            const p_html = createCards(1, document.querySelectorAll('.cards')[1]);
-            cardHtml(this.dataset.cardvalue, p_html[0]);
-            turn = 'Player 1';
-            setTimeout(() => {
-                document.getElementById('p1').innerHTML = '(active)';
-                document.getElementById('p2').innerHTML = '';
-            }, timeout);
-        }
-        document.querySelectorAll('.pick > button').forEach(button => button.disabled = true);
-
-        setTimeout(() => {
-            document.querySelectorAll('.pick > button').forEach(button => button.disabled = false);
-            generate_random_cards();
-            if (p2.hand.length === 5) {
-                document.querySelectorAll('.pick > button').forEach(button => button.remove());
-                pokerHands(p1.hand, p2.hand);
-            }
-        }, timeout);
-
-        setTimeout(() => market.forEach(button => {
-            if (button !== this) flipCard.call(button);
-        }), timeout / 5)
-    }
-
     /**Generate 4 random cards from deck */
     function generate_random_cards() {
         const table = new Player(deck_of_cards);
@@ -83,9 +79,32 @@ function startGame() {
             table.draw_a_card();
             market[i].dataset.cardvalue = table.hand[i];
             cardHtml(table.hand[i], market[i]);
-            flipCard.call(market[i]);
+            flipCard.call(market[i]);  // flip all cards to cover
         }
         table.drop_all_cards();
+    }
+}
+
+/**Use card string to customise html element */
+function cardHtml(card, element) {
+    element.innerHTML = getCardUnicode(card);
+    let class_name;
+    if (card.includes('D') || card.includes('H')) {
+        class_name = 'red card';
+    } else {
+        class_name = 'black card';
+    }
+    element.className = class_name;
+    element.dataset.class0 = class_name;
+}
+
+function flipCard() {
+    if (this.innerHTML.codePointAt() === 127136) {
+        this.innerHTML = getCardUnicode(this.dataset.cardvalue);
+        this.className = this.dataset.class0;
+    } else {
+        this.innerHTML = '&#127136;';
+        this.className = 'back card';
     }
 }
 
@@ -102,76 +121,6 @@ function full_deck() {
     const deck_of_cards = [];
     Array.from('23456789TJQKA').forEach(val => Array.from('SHDC').forEach(suit => deck_of_cards.push(val + suit)));
     return deck_of_cards;
-}
-
-/**Returns the rank of a hand in the card game of poker.*/
-function rank(hand) {
-    if (Array.from('TJQKA').every(i => hand.map(j => j[0]).includes(i)) && isSameSuit()) {
-        return { score: 10, name: 'Royal Flush' };
-
-    } else if (isConsecutive() && isSameSuit()) {
-        return { score: 9, name: 'Straight Flush' };
-
-    } else if (nOfaKind(4) > 0) {
-        return { score: 8, name: 'Four of a Kind' };
-
-    } else if (nOfaKind(3) > 0 && nOfaKind(2)) {
-        return { score: 7, name: 'Full House' };
-
-    } else if (isSameSuit()) {
-        return { score: 6, name: 'Flush' };
-
-    } else if (isConsecutive()) {
-        return { score: 5, name: 'Straight' };
-
-    } else if (nOfaKind(3) > 0) {
-        return { score: 4, name: 'Three of a Kind' };
-
-    } else if (nOfaKind(2) > 1) {
-        return { score: 3, name: 'Two Pairs' };
-
-    } else return nOfaKind(2) > 0 ?
-        { score: 2, name: 'One Pair' } : { score: 1, name: 'High Card' };
-
-    /**Checks if card values are consecutive */
-    function isConsecutive() {
-        const card_values = Array.from('23456789TJQKA');
-        const d = hand.map(i => card_values.indexOf(i[0]));
-        d.sort((a, b) => a - b);
-        const a = d.map(i => card_values[i]).join('');
-        const b = card_values.join('');
-        for (let i = 0; i < b.length - 5; i++) if (a === b.slice(i, i + 5)) return true;
-        return false;
-    }
-
-    /**Returns number of occurrence of n of a kind. */
-    function nOfaKind(n) {
-        const kind = {};
-        for (let i = 0; i < hand.length; i++) {
-            val = hand[i][0];
-            if (!Object.keys(kind).includes(val)) {
-                kind[val] = 1;
-            } else {
-                kind[val]++;
-            }
-        }
-        return count(Object.values(kind), n);
-    }
-
-    /**Checks if cards are same suit. */
-    function isSameSuit() {
-        const hand_suits = hand.map(i => i[1]);
-        const unique = new Set(hand_suits);
-        return Array.from(unique).length === 1;
-    }
-}
-
-/**Converts cards in hand to values in descending order. */
-function handvalue(hand) {
-    const val = hand.map(card => card[0]);
-    const card_values = Array.from('23456789TJQKA');
-    const lst = val.map(v => card_values.indexOf(v) + 2);
-    return lst.map(i => i).sort((x, y) => y - x);
 }
 
 /**Returns the number of occurence of an element in an array */
@@ -201,44 +150,21 @@ function createCards(k, parentElement) {
     return card_array;
 }
 
-/**Use card string to customise html element */
-function cardHtml(card, element) {
-    element.innerHTML = getCardUnicode(card);
-    let class_name;
-    if (card.includes('D') || card.includes('H')) {
-        class_name = 'red card';
-    } else {
-        class_name = 'black card';
-    }
-    element.className = class_name;
-    element.dataset.class0 = class_name;
-}
-
 function getCardUnicode(card) {
-    const card_unicodes = {};
+    const card_unicodes = { cover: '&#127136;' };
     let jj = 0;
     for (let j = 10; j <= 13; j++) {
         let ii = 0;
         for (let i = 1; i <= 14; i++) {
             if (i === 12) continue;
-            const c = 'A23456789TJQK'[ii] + 'SHDC'[jj];
+            const key = 'A23456789TJQK'[ii] + 'SHDC'[jj];
             const hex = `1f0${j.toString(16)}${i.toString(16)}`;
-            card_unicodes[c] = `&#${parseInt(hex, 16)};`;
+            card_unicodes[key] = `&#${parseInt(hex, 16)};`;
             ii++;
         }
         jj++;
     }
     return card_unicodes[card];
-}
-
-function flipCard() {
-    if (this.innerHTML.codePointAt() === 127136) {
-        this.innerHTML = getCardUnicode(this.dataset.cardvalue);
-        this.className = this.dataset.class0;
-    } else {
-        this.innerHTML = '&#127136;';
-        this.className = 'back card';
-    }
 }
 
 function createPokerHtml(result, rName) {
@@ -287,4 +213,77 @@ function pokerHands(hand1, hand2) {
         }
     }
     createPokerHtml(result, [r1.name, r2.name]);
+    return;
+
+    /**Returns the rank of a hand in the card game of poker.*/
+    function rank(hand) {
+        if (Array.from('TJQKA').every(i => hand.map(j => j[0]).includes(i)) && isSameSuit()) {
+            return { score: 10, name: 'Royal Flush' };
+
+        } else if (isConsecutive() && isSameSuit()) {
+            return { score: 9, name: 'Straight Flush' };
+
+        } else if (nOfaKind(4) > 0) {
+            return { score: 8, name: 'Four of a Kind' };
+
+        } else if (nOfaKind(3) > 0 && nOfaKind(2)) {
+            return { score: 7, name: 'Full House' };
+
+        } else if (isSameSuit()) {
+            return { score: 6, name: 'Flush' };
+
+        } else if (isConsecutive()) {
+            return { score: 5, name: 'Straight' };
+
+        } else if (nOfaKind(3) > 0) {
+            return { score: 4, name: 'Three of a Kind' };
+
+        } else if (nOfaKind(2) > 1) {
+            return { score: 3, name: 'Two Pairs' };
+
+        } else return nOfaKind(2) > 0 ?
+            { score: 2, name: 'One Pair' } : { score: 1, name: 'High Card' };
+
+        /**Checks if card values are consecutive */
+        function isConsecutive() {
+            const card_values = Array.from('23456789TJQKA');
+            const d = hand.map(i => card_values.indexOf(i[0]));
+            d.sort((a, b) => a - b);
+            const a = d.map(i => card_values[i]).join('');
+            const b = card_values.join('');
+            for (let i = 0; i < b.length - 5; i++) if (a === b.slice(i, i + 5)) return true;
+            return false;
+        }
+
+        /**Returns number of occurrence of n of a kind. */
+        function nOfaKind(n) {
+            const kind = {};
+            for (let i = 0; i < hand.length; i++) {
+                val = hand[i][0];
+                if (!Object.keys(kind).includes(val)) {
+                    kind[val] = 1;
+                } else {
+                    kind[val]++;
+                }
+            }
+            return count(Object.values(kind), n);
+        }
+
+        /**Checks if cards are same suit. */
+        function isSameSuit() {
+            const hand_suits = hand.map(i => i[1]);
+            const unique = new Set(hand_suits);
+            return Array.from(unique).length === 1;
+        }
+    }
+
+    /**Converts cards in hand to values in descending order. */
+    function handvalue(hand) {
+        const val = hand.map(card => card[0]);
+        const card_values = Array.from('23456789TJQKA');
+        const lst = val.map(v => card_values.indexOf(v) + 2);
+        return lst.map(i => i).sort((x, y) => y - x);
+    }
 }
+
+startGame();
